161c81a8fa24e6872ff12b683a3fbd53a9532595
Vadim <kv1o@yandex.ru>
2021-09-01 19:10:37 +0300

    add Lookup

--- src/main/java/com/example/music/annotation/DigitalMusicPlayer.java
+++ src/main/java/com/example/music/annotation/DigitalMusicPlayer.java
@@ -1,12 +1,19 @@
 package com.example.music.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 @Component
 public class DigitalMusicPlayer implements MusicPlayer {
-    private final Music music;
+    @Autowired
+    @Qualifier("rockMusic")
+    private Music music;
+
+    public DigitalMusicPlayer() {}
 
     @Autowired
     public DigitalMusicPlayer(
@@ -14,7 +21,14 @@ public class DigitalMusicPlayer implements MusicPlayer {
         this.music = music;
     }
 
+    @Autowired
+    @Qualifier("rockMusic")
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     @Override
+    @Lookup("rockMusic")
     public Music getMusic() {
         return music;
     }
